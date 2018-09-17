e5e1c210a0ff06c2e7640e8eba6efcfb6d3922a1
Nicole Carmack <nicole@cpe-2606-a000-ee8f-9000-7456-714c-d444-a583.dyn6.twc.com>
2018-09-17 17:17:47 -0400

    Initial Commit

---
+++ uvc/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  uvc
+//
+//  Created by Nicole Carmack on 2018-09-17.
+//  Copyright © 2018 Nicole Carmack. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
